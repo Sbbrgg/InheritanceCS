5fc2312422606ce57540c1d94948cd2590f4beab
un <un@DESKTOP-JAU235M>
2025-11-03 21:43:26 +0300

    Add project files.

---
+++ Academy/Human.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Human
+	{
+		public string LastName {  get; set; }
+		public string FirstName { get; set; }
+		public int Age { get; set; }
+
+		public Human(string lastName, string firstName, int age)
+		{
+			LastName = lastName;
+			FirstName = firstName;
+			Age = age;
+			Console.WriteLine($"HConstructor:{GetHashCode()}");
+		}
+		public Human(Human other)
+		{
+			this.LastName = other.LastName;
+			this.FirstName = other.FirstName;
+			this.Age = other.Age;
+			Console.WriteLine($"CopyConstructor:\t{GetHashCode()}");
+		}
+		~Human()
+		{
+			Console.WriteLine($"HDestructor:{GetHashCode()}");
+		}
+
+		public virtual void Info()
+		{
+			Console.WriteLine($"{LastName} {FirstName} {Age}");
+		}
+		public override string ToString()
+		{
+			return 
+				//Split('.') разделяет 'Academy.Type' по точке на массив строк,
+				//и из этого мы берём последний элемент.
+				$"{base.ToString().Split('.').Last()}:".PadRight(12) +
+				$" {LastName.PadRight(16, '.')} {FirstName.PadRight(10)} {Age.ToString().PadRight(5)}";
+			//PadRight() выравнивает строку по левому борту. От Padding - выравнивание/набивка.
+			//"Набивает справа от нашего значения"
+		}
+	}
+}
---
+++ Academy/Teacher.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy
+{
+	internal class Teacher: Human
+	{
+		public string Speciality { get; set; }
+		public int Expirience { get; set; }
+		public Teacher
+			(
+				string lastName, string firstName, int age,
+				string specility, int experience
+			): base(lastName, firstName, age)
+		{
+			Speciality = specility;
+			Expirience = experience;
+			Console.WriteLine($"TConstructor:\t{GetHashCode()}");
+		}
+		public Teacher
+			(
+				Human human, string specility, int experience) : base(human)
+		{
+			Speciality = specility;
+			Expirience = experience;
+		}
+		~Teacher()
+		{
+			Console.WriteLine($"TDestructor:\t{GetHashCode()}");
+		}
+
+		public override void Info()
+		{
+			base.Info();
+			Console.WriteLine($"{Speciality} {Expirience}");
+		}
+		//public override string ToString()
+		//{
+		//	return base.ToString() +
+		//	$"";
+		//}
+	}
+}
