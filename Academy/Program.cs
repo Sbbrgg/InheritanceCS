b27f1c3c21c0c9e5f3af8eeefe9c7f74aee0cd39
un <un@DESKTOP-JAU235M>
2025-11-05 21:03:09 +0300

    Streamer DONE

--- Academy/Program.cs
+++ Academy/Program.cs
@@ -16,6 +16,7 @@ namespace Academy
 		static readonly string delimiter = "\n------------------------------------------------------------------------------------------------------------------\n";
 		static void Main(string[] args)
 		{
+
 #if INHERITANCE_1
 			Console.WriteLine("Academy");
 			Human human = new Human("Montana", "Antonio", 25);
@@ -47,6 +48,7 @@ namespace Academy
 			graduate.Info();
 			Console.WriteLine(delimiter);
 #endif
+			Streamer streamer = new Streamer();
 #if WRITE_TO_FILE
 
 			/*Base-class pointers: Generalisation (Upcast - привидение дочернего объекта)
@@ -69,70 +71,12 @@ namespace Academy
 				Console.WriteLine(group[i]);
 				Console.WriteLine(delimiter);
 			}
-			Save(group, "group.txt"); 
+			streamer.Save(group, "group.txt"); 
 #endif
 
-			Human[] group = Load("group.txt");
-			Print(group);
+			Human[] group = streamer.Load("group.txt");
+			streamer.Print(group);
 
-		}
-		static void Save(Human[] group, string filename)
-		{
-			StreamWriter writer = new StreamWriter(filename);
-
-			for (int i = 0; i < group.Length; i++)
-			{
-				writer.WriteLine(group[i].ToStringCSV());
-			}
-
-			writer.Close();
-			System.Diagnostics.Process.Start("notepad", filename);
-		}
-		static void Print(Human[] group)
-		{
-			for (int i = 0; i < group.Length; i++)
-			{
-				Console.WriteLine(group[i]);
-				Console.WriteLine(delimiter);
-			}
-			Console.WriteLine();
-		}
-		static Human[] Load(string filename)
-		{
-			List<Human> group = new List<Human>();
-			StreamReader reader = new StreamReader(filename);
-			try
-			{
-				while (!reader.EndOfStream)
-				{
-					string buffer = reader.ReadLine();
-					string[] values = buffer.Split(',');
-					//Human human = HumanFactory(values.First());
-					//human.Init(values);
-					//group.Add(human);
-
-					group.Add(HumanFactory(values[0]).Init(values));
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine(ex.Message);
-				throw;
-			}
-			reader.Close();
-			return group.ToArray();
-		}
-		static Human HumanFactory(string type)
-		{
-			Human human = null;
-			switch (type)
-			{
-				case "Human": human = new Human("", "", 0); break;
-				case "Student": human = new Student("", "", 0, "", "", 0, 0); break;
-				case "Graduate": human = new Graduate("", "", 0, "", "", 0, 0, ""); break;
-				case "Teacher": human = new Teacher("", "", 0, "", 0); break;
-			}
-			return human;
 		}
 	}
 }
