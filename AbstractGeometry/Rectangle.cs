ba1857726dfc9a138bd22e5cd4def1ff5a2686b7
un <un@DESKTOP-JAU235M>
2025-11-10 03:10:39 +0300

    rectangle Draw DONE

---
+++ AbstractGeometry/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractGeometry
+{
+	internal class Rectangle: Shape
+	{
+		double width;
+		double height;
+
+		public double Width
+		{
+			get => width;
+			set => width = FilterSize(value);
+		}
+		public double Height
+		{
+			get => height;
+			set => height = FilterSize(value);
+		}
+		public Rectangle
+			(
+				double width, double height,
+				int startX, int startY, int lineWidth, Color color
+			): base( startX, startY, lineWidth, color )
+		{
+			Width = width;
+			Height = height;
+		}
+		public override double GetArea() => width * height;
+		public override double GetPerimeter() => 2 * (width + height);
+		public override void Draw(System.Windows.Forms.PaintEventArgs e)
+		{
+			Pen pen = new Pen(Color.White);
+			SolidBrush brush = new SolidBrush(Color);
+			e.Graphics.DrawRectangle(pen, StartX, StartY, (float)Width, (float)Height);
+			e.Graphics.FillRectangle(brush, StartX, StartY, (float)Width, (int)Height);
+		}
+		public override void Info(System.Windows.Forms.PaintEventArgs e)
+		{
+			Console.WriteLine(this.GetType().ToString().Split('.').Last() + ":");
+			Console.WriteLine($"Ширина: {Width}");
+			Console.WriteLine($"Ширина: {Height}");
+			base.Info(e);
+		}
+	}
+}
